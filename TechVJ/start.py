2953114fdbb7838adb1e2e83ae4576d7b407b035
veer708 <veerahirwal51@gmail.com>
2025-12-30 13:23:33 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -61,7 +61,7 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/LUCKY_y3")
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/LUCKY_y3Bot")
     ],[
         InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/+00WJNYJVl2ExNjZl'),
         InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/+00WJNYJVl2ExNjZl')
