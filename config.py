c9248754ef7b737886d8c440da7d21f4c3ce88ab
veer708 <veerahirwal51@gmail.com>
2026-02-19 12:01:16 +0530

    Update BOT_TOKEN in config.py
    Replaced placeholder BOT_TOKEN with actual token.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8332937403:AAGklBjk89Tu2MNHOyiycQsVupfqqcARRZ4")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "22216834"))
